b6d5fb773e7eeb34ece2a54e89fda3ce4bbbdc14
Manfred Schreiber <manfred.schreiber@jambit.com>
2024-03-28 13:14:33 +0100

    .

--- main.go
+++ main.go
@@ -12,7 +12,14 @@ import (
 func main() {
 	var inputFile string
 	var pathPrefix string
-	textMap := map[string]string{}
+	var textMap struct {
+		changes map[string]bool
+		log     map[string]string
+	}
+
+	textMap.log = map[string]string{}
+	textMap.changes = map[string]bool{}
+
 	flag.StringVar(&inputFile, "f", "", "Input File")
 	flag.StringVar(&pathPrefix, "p", "", "Prefix to remove")
 	flag.Parse()
@@ -26,16 +33,38 @@ func main() {
 	fileScanner.Split(bufio.ScanLines)
 	r, _ := regexp.Compile(`^\[(.*?)\]\s(.*)$`)
 	for fileScanner.Scan() {
-		x := r.FindStringSubmatch(fileScanner.Text())
+		line := fileScanner.Text()
+		x := r.FindStringSubmatch(line)
+		if len(x) != 3 {
+			fmt.Println(line)
+			continue
+		}
 		key := x[1]
 		value := x[2]
-		textMap[key] += fmt.Sprintf("%s\n", value)
+		if strings.Contains(value, "will perform the following actions") {
+			textMap.changes[key] = true
+		}
+		textMap.log[key] += fmt.Sprintf("%s\n", value)
 	}
 
 	readFile.Close()
-	for k, v := range textMap {
-		fmt.Printf("####[group] Module: %s\n\n\n", strings.TrimPrefix(k, pathPrefix))
+
+	fmt.Println("##[section] Changes")
+	for k, v := range textMap.log {
+		if _, ok := textMap.changes[k]; !ok {
+			continue
+		}
+		fmt.Printf("##[group] Module: %s\n\n\n", strings.TrimPrefix(k, pathPrefix))
 		fmt.Println(v)
-		fmt.Printf("\n\n\n####[endgroup]\n")
+		fmt.Printf("\n\n\n##[endgroup]\n")
+	}
+	fmt.Println("##[section] No Changes")
+	for k, v := range textMap.log {
+		if _, ok := textMap.changes[k]; ok {
+			continue
+		}
+		fmt.Printf("##[group] Module: %s\n\n\n", strings.TrimPrefix(k, pathPrefix))
+		fmt.Println(v)
+		fmt.Printf("\n\n\n##[endgroup]\n")
 	}
 }
